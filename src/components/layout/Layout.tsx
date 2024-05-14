abd5dda9f6cbf97be496633538f0a123a62e66db
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-13 21:38:55 -0500

    feat: :sparkles: add Layout section

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <div>
-        Layout
-      </div>
+      <header className="bg-slate-800 py-16">
+        <div className="container flex justify-between items-center">
+          <div className="w-32">
+            <img src="/logo.svg" alt="" />
+          </div>
+          <div>
+            <nav className="space-x-4">
+              <Link to="/" className="text-white font-semibold uppercase">
+                Inicio
+              </Link>
+
+              <Link to="/favorites" className="text-white font-semibold uppercase">
+                Favoritos
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
 
       <main>
         <Outlet />
