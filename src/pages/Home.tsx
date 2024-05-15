34c2c96521e1d04e367112da95ed2c2686d2b7d8
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-15 00:25:24 -0500

    feat: :sparkles: add dynamic render of every recipe in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import { Recipe } from "@/types"
+import { Recipe } from "@/types";
 
 interface HomeProps {
   recipes: Recipe[]
+}
+
+function Card({ recipe }: { recipe: Recipe }) {
+  return (
+    <div className="basis-auto sm:basis-[49%] lg:basis-[32%] border shadow-lg">
+      <div className="overflow-hidden">
+        <img loading="lazy" src={recipe.strDrinkThumb} alt={recipe.strDrink} className="hover:rotate-6 hover:scale-125 transition-transform" />
+      </div>
+      <div className="p-4 space-y-4">
+        <h3 className="text-xl font-semibold">{recipe.strDrink}</h3>
+        <button className="w-full bg-orange-400 hover:bg-orange-500 cursor-pointer text-center py-2 uppercase text-white font-semibold">
+          Ver Receta
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default function Home({ recipes }: HomeProps) {
@@ -14,11 +30,11 @@ export default function Home({ recipes }: HomeProps) {
 
         {
           recipes.length ? (
-            recipes.map((recipe) => (
-              <div key={recipe.idDrink}>
-                {recipe.idDrink} - {recipe.strDrink}
-              </div>
-            ))
+            <div className="w-full flex flex-wrap justify-between items-center gap-y-5">
+              {recipes.map((recipe) => (
+                <Card key={recipe.idDrink} recipe={recipe} />
+              ))}
+            </div>
           ) : (
             <p className="text-center text-2xl">
               ¡Aún no hay resultados! Utiliza el formulario para buscar recetas
