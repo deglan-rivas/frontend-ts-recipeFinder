181d2bdc8db105878391e56640ccfe81f9a2e7c6
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-14 19:11:23 -0500

    feat: :sparkles: add main section to favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,15 @@
 export default function Favorites() {
   return (
-    <div>
-      Favorites
-    </div>
+    <>
+      <div className="space-y-10">
+        <h2 className="text-6xl font-semibold">
+          Favoritos
+        </h2>
+
+        <p className="text-center text-2xl">
+          Tus favoritos se mostrarán aquí
+        </p>
+      </div>
+    </>
   )
 }
